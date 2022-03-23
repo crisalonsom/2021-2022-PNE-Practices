e9d613f5f8e25c2c267a722674ef5cd6b341c07d
kitia <c.alonsom.2021@alumnos.urjc.es>
2022-03-23 13:58:50 +0100

    p1

--- P1/Ex10.py
+++ P1/Ex10.py
@@ -6,8 +6,15 @@ for gene in GENES :
     try:
         seq = Seq()
         seq.read_fasta(filename)
-        max_base = seq.most_frequent()
-        print(f"Gene {gene}: {max_base[1]}")
+        bases_appearances = seq.count()
+        most_frequent = None
+        for base,count in bases_appearances.items():
+            if most_frequent:
+                if count > most_frequent[1]:
+                    most_frequent = (base,count)
+            else:
+                most_frequent = (base,count)
+        print(f"Gene {gene}: {most_frequent[0]}")
     except FileNotFoundError:
         print(f"[ERROR]: file '{filename}' not found ")
 
