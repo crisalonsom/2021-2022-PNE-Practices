e9a530560506fc4e01c46dfa9c9c73769fe28311
kitia <c.alonsom.2021@alumnos.urjc.es>
2022-04-18 08:54:41 +0200

    Session14

---
+++ Session14/webserver1.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+
+PORT = 8080
+socketserver.TCPServer.allow_reuse_address = True
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("Server Stopped!")
+        httpd.server_close()
