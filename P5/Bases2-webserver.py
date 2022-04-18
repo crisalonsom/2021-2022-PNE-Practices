28914836b8821efdf1eed91aeb5b656bb46ac2c4
kitia <c.alonsom.2021@alumnos.urjc.es>
2022-04-18 09:07:57 +0200

    P5

---
+++ P5/Bases2-webserver.py
@@ -0,0 +1,47 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+
+PORT = 8080
+
+
+socketserver.TCPServer.allow_reuse_address = True
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_get(self):
+        termcolor.cprint(self.requestline, 'green')
+        if self.path == "/" or self.path == "/index.html":
+            contents = Path("index.html").read_text()
+            self.send_response(200)
+        else:
+            resource = self.path[1:]
+            try:
+                contents = Path(f"{resource}").read_text()
+                self.send_response(200)
+            except FileNotFoundError:
+                contents = Path("Error.html").read_text()
+                self.send_response(404)
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', str(len(contents.encode())))
+        self.end_headers()
+        self.wfile.write(contents.encode())
+
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
